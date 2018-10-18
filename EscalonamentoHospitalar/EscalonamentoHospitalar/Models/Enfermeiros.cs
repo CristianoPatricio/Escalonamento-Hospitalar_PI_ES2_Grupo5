1fc84f7cca8a6f7b3f2862fe2455fd728c90fa06
Nelson Monteiro <nsqlmonteiro@gmail.com>
2018-10-18 22:26:20 +0100

    criação dos modelos para medicos e enfermeiros

---
+++ EscalonamentoHospitalar/EscalonamentoHospitalar/Models/Enfermeiros.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EscalonamentoHospitalar.Models
+{
+    public class Enfermeiros : Controller
+    {
+        public string EnfermeiroID{ get; set; }
+
+        public string Codigo_Enfermeiro{ get; set; }
+
+        public string Nome { get; set; }
+
+        public string Telemovel { get; set; }
+
+        public string Email { get; set; }
+
+        public string Morada { get; set; }
+
+        public string Cod_Postal { get; set; }
+
+        
+    }
+}
+
---
+++ EscalonamentoHospitalar/EscalonamentoHospitalar/Models/Medicos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EscalonamentoHospitalar.Models
+{
+    public class Medicos : Controller
+    {
+        public string MedicoID { get; set; }
+
+        public string Codigo_Medico { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Telemovel { get; set; }
+
+        public string Email { get; set; }
+
+        public string Morada { get; set; }
+
+        public string Cod_Postal { get; set; }
+    }
+}
