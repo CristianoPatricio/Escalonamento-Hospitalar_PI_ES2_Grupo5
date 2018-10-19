bc4e5cd07b05d07eb3f2c919bbdb521624d3ada3
Cristiano Patrício <cristianop1998@gmail.com>
2018-10-19 11:04:47 +0100

    modified layout of login page

--- EscalonamentoHospitalar/EscalonamentoHospitalar/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ EscalonamentoHospitalar/EscalonamentoHospitalar/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -37,8 +37,7 @@ namespace EscalonamentoHospitalar.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [EmailAddress]
-            public string Email { get; set; }
+            public string Código { get; set; }
 
             [Required]
             [DataType(DataType.Password)]
@@ -73,7 +72,7 @@ namespace EscalonamentoHospitalar.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
+                var result = await _signInManager.PasswordSignInAsync(Input.Código, Input.Password, Input.RememberMe, lockoutOnFailure: true);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
