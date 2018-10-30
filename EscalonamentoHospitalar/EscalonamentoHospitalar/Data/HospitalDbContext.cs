d043547832c4839f6cf152f147bcdebfed3f361d
Cristiano Patrício <cristianop1998@gmail.com>
2018-10-30 12:23:58 +0000

    Scaffolding CRUD DiretorServico (30/10/2018)

---
+++ EscalonamentoHospitalar/EscalonamentoHospitalar/Data/HospitalDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EscalonamentoHospitalar.Models
+{
+    public class HospitalDbContext : DbContext
+    {
+        public HospitalDbContext (DbContextOptions<HospitalDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EscalonamentoHospitalar.Models.DiretorServico> DiretorServico { get; set; }
+    }
+}
