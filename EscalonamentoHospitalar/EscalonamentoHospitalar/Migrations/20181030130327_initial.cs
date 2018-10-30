e47c4154f827d483a4806e7dc4b9301debf4b08a
Cristiano Patrício <cristianop1998@gmail.com>
2018-10-30 13:05:52 +0000

    Criação da DB Hospital (30/10/2018)

---
+++ EscalonamentoHospitalar/EscalonamentoHospitalar/Migrations/20181030130327_initial.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EscalonamentoHospitalar.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DiretorServico",
+                columns: table => new
+                {
+                    DiretorServicoID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false),
+                    NumeroMecanografico = table.Column<string>(nullable: true),
+                    Contacto = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    CC = table.Column<string>(nullable: true),
+                    Morada = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiretorServico", x => x.DiretorServicoID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Enfermeiros",
+                columns: table => new
+                {
+                    EnfermeiroID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    NumeroMecanografico = table.Column<string>(nullable: true),
+                    Nome = table.Column<string>(nullable: false),
+                    Especialidade = table.Column<string>(nullable: true),
+                    Contacto = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: false),
+                    Data_Nascimento = table.Column<DateTime>(nullable: false),
+                    CC = table.Column<string>(nullable: true),
+                    EspecialidadeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enfermeiros", x => x.EnfermeiroID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DiretorServico");
+
+            migrationBuilder.DropTable(
+                name: "Enfermeiros");
+        }
+    }
+}
