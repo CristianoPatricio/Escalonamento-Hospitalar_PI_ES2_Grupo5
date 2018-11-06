a41da839dfc66f2397f010e39ba9070ccd5d52be
Cristiano Patrício <cristianop1998@gmail.com>
2018-11-06 13:09:34 +0000

    Scaffolding Enfermeiros
    06/11/2018

---
+++ EscalonamentoHospitalar/EscalonamentoHospitalar/Controllers/EnfermeirosController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EscalonamentoHospitalar.Models;
+
+namespace EscalonamentoHospitalar.Controllers
+{
+    public class EnfermeirosController : Controller
+    {
+        private readonly HospitalDbContext _context;
+
+        public EnfermeirosController(HospitalDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Enfermeiros
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Enfermeiro.ToListAsync());
+        }
+
+        // GET: Enfermeiros/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var enfermeiro = await _context.Enfermeiro
+                .FirstOrDefaultAsync(m => m.EnfermeiroId == id);
+            if (enfermeiro == null)
+            {
+                return NotFound();
+            }
+
+            return View(enfermeiro);
+        }
+
+        // GET: Enfermeiros/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Enfermeiros/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EnfermeiroId,NumeroMecanografico,Nome,Contacto,Email,Data_Nascimento,CC")] Enfermeiro enfermeiro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(enfermeiro);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(enfermeiro);
+        }
+
+        // GET: Enfermeiros/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var enfermeiro = await _context.Enfermeiro.FindAsync(id);
+            if (enfermeiro == null)
+            {
+                return NotFound();
+            }
+            return View(enfermeiro);
+        }
+
+        // POST: Enfermeiros/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EnfermeiroId,NumeroMecanografico,Nome,Contacto,Email,Data_Nascimento,CC")] Enfermeiro enfermeiro)
+        {
+            if (id != enfermeiro.EnfermeiroId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(enfermeiro);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EnfermeiroExists(enfermeiro.EnfermeiroId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(enfermeiro);
+        }
+
+        // GET: Enfermeiros/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var enfermeiro = await _context.Enfermeiro
+                .FirstOrDefaultAsync(m => m.EnfermeiroId == id);
+            if (enfermeiro == null)
+            {
+                return NotFound();
+            }
+
+            return View(enfermeiro);
+        }
+
+        // POST: Enfermeiros/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var enfermeiro = await _context.Enfermeiro.FindAsync(id);
+            _context.Enfermeiro.Remove(enfermeiro);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EnfermeiroExists(int id)
+        {
+            return _context.Enfermeiro.Any(e => e.EnfermeiroId == id);
+        }
+    }
+}
