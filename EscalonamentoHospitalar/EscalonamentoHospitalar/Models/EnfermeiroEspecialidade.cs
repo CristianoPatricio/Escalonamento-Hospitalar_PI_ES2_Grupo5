3ae057630710c4ff6411660d097ee8ed82d951d8
Cristiano Patrício <cristianop1998@gmail.com>
2018-11-07 12:54:55 +0000

    Alteração de campos nos modelos MedicoEspecialidade, EnfermeiroEspecialidade
    07/11/2018

--- EscalonamentoHospitalar/EscalonamentoHospitalar/Models/EnfermeiroEspecialidade.cs
+++ EscalonamentoHospitalar/EscalonamentoHospitalar/Models/EnfermeiroEspecialidade.cs
@@ -7,13 +7,11 @@ namespace EscalonamentoHospitalar.Models
 {
     public class EnfermeiroEspecialidade
     {
+
         public int EnfermeiroEspecialidadeId { get; set; }
+        public string Nome { get; set; }
 
-        public int EnfermeiroId { get; set; }
         public Enfermeiro Enfermeiro { get; set; }
-
-        public int EspecilidadeId { get; set; }
-        public Especialidade Especialidade { get; set; }
-
+        public int EnfermeiroId { get; set; }
     }
 }
