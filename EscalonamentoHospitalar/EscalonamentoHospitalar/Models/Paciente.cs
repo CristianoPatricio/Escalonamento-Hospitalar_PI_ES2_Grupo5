2d84f3ed14926d0e2e8547414c13be927028c90c
Nelson Monteiro <nsqlmonteiro@gmail.com>
2018-11-13 09:00:57 +0000

    validação dos atributos

--- EscalonamentoHospitalar/EscalonamentoHospitalar/Models/Paciente.cs
+++ EscalonamentoHospitalar/EscalonamentoHospitalar/Models/Paciente.cs
@@ -25,6 +25,7 @@ namespace EscalonamentoHospitalar.Models
         [RegularExpression(@"(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})", ErrorMessage = "Email Inválido")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Numero Inválido")]
         [RegularExpression(@"\d{8}(\s\d{1})?", ErrorMessage = "Cartão de Cidadão Inválido")]
         public string CC { get; set; }
 
@@ -33,6 +34,7 @@ namespace EscalonamentoHospitalar.Models
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = false)]
         public DateTime Data_Nascimento { get; set; }
 
+        [Required(ErrorMessage = "Numero Inválido")]
         [RegularExpression(@"(\d{9})", ErrorMessage = "Nº Utente de Saúde Inválido")]
         public string Numero_Utente { get; set; }
 
