59e440087120eef24dbcf47b569039cdbce63cbf
Cristiano Patrício <cristianop1998@gmail.com>
2018-12-04 11:40:21 +0000

    Criação do Modelo GerarHorarioEnfermeiro 4/12/2018
    Este modelo vai servir de suporte para gerar horários automáticamente para os enfermeiros.

---
+++ EscalonamentoHospitalar/EscalonamentoHospitalar/Models/GerarHorarioEnfermeiro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EscalonamentoHospitalar.Models
+{
+    public class GerarHorarioEnfermeiro
+    {
+        [Required(ErrorMessage = "Por favor, introduza o número de pessoas para o turno 1")]
+        public int NumeroPessoasTurno1 { get; set; }
+
+        [Required(ErrorMessage = "Por favor, introduza o número de pessoas para o turno 2")]
+        public int NumeroPessoasTurno2 { get; set; }
+
+        [Required(ErrorMessage = "Por favor, introduza o número de pessoas para o turno 3")]
+        public int NumeroPessoasTurno3 { get; set; }
+
+        [DataType(DataType.Date, ErrorMessage = "Por favor, indique a data de ínicio da semana")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = false)]
+        public DateTime DataInicioSemana { get; set; }
+    }
+}
