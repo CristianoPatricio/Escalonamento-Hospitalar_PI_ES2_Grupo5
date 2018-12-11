b0e732659b1cbd7a5b14d991e18c3de8d5bdf5b6
Cristiano Patrício <cristianop1998@gmail.com>
2018-12-11 12:15:13 +0000

    Adicionado Modelo 'PedidoTrocaTurnoEnfermeiro'

---
+++ EscalonamentoHospitalar/EscalonamentoHospitalar/Controllers/PedidoTrocaTurnosEnfermeirosController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EscalonamentoHospitalar.Models;
+
+namespace EscalonamentoHospitalar.Controllers
+{
+    public class PedidoTrocaTurnosEnfermeirosController : Controller
+    {
+        private readonly HospitalDbContext _context;
+
+        public PedidoTrocaTurnosEnfermeirosController(HospitalDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: PedidoTrocaTurnosEnfermeiros
+        public async Task<IActionResult> Index()
+        {
+            var hospitalDbContext = _context.PedidoTrocaTurnosEnfermeiro.Include(p => p.EstadoPedidoTroca);
+            return View(await hospitalDbContext.ToListAsync());
+        }
+
+        // GET: PedidoTrocaTurnosEnfermeiros/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedidoTrocaTurnosEnfermeiro = await _context.PedidoTrocaTurnosEnfermeiro
+                .Include(p => p.EstadoPedidoTroca)
+                .FirstOrDefaultAsync(m => m.PedidoTrocaTurnosEnfermeiroId == id);
+            if (pedidoTrocaTurnosEnfermeiro == null)
+            {
+                return NotFound();
+            }
+
+            return View(pedidoTrocaTurnosEnfermeiro);
+        }
+
+        // GET: PedidoTrocaTurnosEnfermeiros/Create
+        public IActionResult Create()
+        {
+            ViewData["EstadoPedidoTrocaId"] = new SelectList(_context.EstadoPedidoTrocas, "EstadoPedidoTrocaId", "EstadoPedidoTrocaId");
+            return View();
+        }
+
+        // POST: PedidoTrocaTurnosEnfermeiros/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PedidoTrocaTurnosEnfermeiroId,DataPedido,EnfermeiroRequerenteId,HorarioATrocarId,EnfermeiroATrocarId,HorarioParaTrocaId,EnfermeiroParaTrocaId,EstadoPedidoTrocaId")] PedidoTrocaTurnosEnfermeiro pedidoTrocaTurnosEnfermeiro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pedidoTrocaTurnosEnfermeiro);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EstadoPedidoTrocaId"] = new SelectList(_context.EstadoPedidoTrocas, "EstadoPedidoTrocaId", "EstadoPedidoTrocaId", pedidoTrocaTurnosEnfermeiro.EstadoPedidoTrocaId);
+            return View(pedidoTrocaTurnosEnfermeiro);
+        }
+
+        // GET: PedidoTrocaTurnosEnfermeiros/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedidoTrocaTurnosEnfermeiro = await _context.PedidoTrocaTurnosEnfermeiro.FindAsync(id);
+            if (pedidoTrocaTurnosEnfermeiro == null)
+            {
+                return NotFound();
+            }
+            ViewData["EstadoPedidoTrocaId"] = new SelectList(_context.EstadoPedidoTrocas, "EstadoPedidoTrocaId", "EstadoPedidoTrocaId", pedidoTrocaTurnosEnfermeiro.EstadoPedidoTrocaId);
+            return View(pedidoTrocaTurnosEnfermeiro);
+        }
+
+        // POST: PedidoTrocaTurnosEnfermeiros/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PedidoTrocaTurnosEnfermeiroId,DataPedido,EnfermeiroRequerenteId,HorarioATrocarId,EnfermeiroATrocarId,HorarioParaTrocaId,EnfermeiroParaTrocaId,EstadoPedidoTrocaId")] PedidoTrocaTurnosEnfermeiro pedidoTrocaTurnosEnfermeiro)
+        {
+            if (id != pedidoTrocaTurnosEnfermeiro.PedidoTrocaTurnosEnfermeiroId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pedidoTrocaTurnosEnfermeiro);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PedidoTrocaTurnosEnfermeiroExists(pedidoTrocaTurnosEnfermeiro.PedidoTrocaTurnosEnfermeiroId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EstadoPedidoTrocaId"] = new SelectList(_context.EstadoPedidoTrocas, "EstadoPedidoTrocaId", "EstadoPedidoTrocaId", pedidoTrocaTurnosEnfermeiro.EstadoPedidoTrocaId);
+            return View(pedidoTrocaTurnosEnfermeiro);
+        }
+
+        // GET: PedidoTrocaTurnosEnfermeiros/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedidoTrocaTurnosEnfermeiro = await _context.PedidoTrocaTurnosEnfermeiro
+                .Include(p => p.EstadoPedidoTroca)
+                .FirstOrDefaultAsync(m => m.PedidoTrocaTurnosEnfermeiroId == id);
+            if (pedidoTrocaTurnosEnfermeiro == null)
+            {
+                return NotFound();
+            }
+
+            return View(pedidoTrocaTurnosEnfermeiro);
+        }
+
+        // POST: PedidoTrocaTurnosEnfermeiros/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pedidoTrocaTurnosEnfermeiro = await _context.PedidoTrocaTurnosEnfermeiro.FindAsync(id);
+            _context.PedidoTrocaTurnosEnfermeiro.Remove(pedidoTrocaTurnosEnfermeiro);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PedidoTrocaTurnosEnfermeiroExists(int id)
+        {
+            return _context.PedidoTrocaTurnosEnfermeiro.Any(e => e.PedidoTrocaTurnosEnfermeiroId == id);
+        }
+    }
+}
---
+++ EscalonamentoHospitalar/EscalonamentoHospitalar/Models/PedidoTrocaTurnosEnfermeiro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EscalonamentoHospitalar.Models
+{
+    public class PedidoTrocaTurnosEnfermeiro
+    {
+        public int PedidoTrocaTurnosEnfermeiroId { get; set; }
+
+        public DateTime DataPedido { get; set; }
+
+        public Enfermeiro Enfermeiro { get; set; }
+        public int EnfermeiroRequerenteId { get; set; }
+
+        public HorarioEnfermeiro HorarioEnfermeiro { get; set; }
+        public int HorarioATrocarId { get; set; }
+        public int EnfermeiroATrocarId { get; set; }
+
+        public int HorarioParaTrocaId { get; set; }     
+        public int EnfermeiroParaTrocaId { get; set; }
+
+        public EstadoPedidoTroca EstadoPedidoTroca { get; set; }
+        public int EstadoPedidoTrocaId { get; set; }     
+    }
+}
