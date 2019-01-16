2ffbf367768e0bafe9f1886d7aab8adfcefef6f0
Nelson Monteiro <nsqlmonteiro@gmail.com>
2019-01-16 19:37:27 +0000

    Ultimas alteraçoes
    Ultimas alteraçoes
    16-01-2019

---
+++ EscalonamentoHospitalar/EscalonamentoHospitalar/Models/ListaTratamentosViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EscalonamentoHospitalar.Models
+{
+    public class ListaTratamentosViewModel
+    {
+        public IEnumerable<Tratamento> Tratamentos { get; set; }
+        public PagingViewModel Pagination { get; set; }
+
+        public string CurrentNome { get; set; }
+    }
+}
